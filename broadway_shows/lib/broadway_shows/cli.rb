fd6dab7e89d2ec2ba12b9650b58ed2103e624c97
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-04 21:16:21 +0000

    Testing

--- broadway_shows/lib/broadway_shows/cli.rb
+++ broadway_shows/lib/broadway_shows/cli.rb
@@ -6,6 +6,17 @@ class BroadwayShows::CLI
   end
 
   def list_shows
+    puts <<-DOC.gsub /^\s*/, ''
+    1. The Boys in the Band - from $89 - available through 5/30
+    2. Harry Potter and the Cursed Child - from $40 - available through lottery
+    3. Waitress - from $89 - showing daily bar Monday
+    4. Mean Girls - from $99 - available through 5/31
+    5. Harry Clarke - from $59 - showing daily bar Monday
+    6. The Play That Goes Wrong - from $30 - showing daily bar Monday and Wednesday
+    7. The Band's Visit - from $49 - showing daily bar Monday
+    8. Bernadetter Peters in Hello, Dolly - from $39 - available 5/13
+    9. Angels in America: Millenium Approaches - from $50 - available through 5/31
+    DOC
   end
 
 end
