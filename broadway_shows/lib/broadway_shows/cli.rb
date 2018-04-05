ddccd8da09833bea67d93cef3ba53f0e500fc9eb
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-05 13:16:43 +0000

    stubbed out more CLI

--- broadway_shows/lib/broadway_shows/cli.rb
+++ broadway_shows/lib/broadway_shows/cli.rb
@@ -1,11 +1,13 @@
 class BroadwayShows::CLI
 
   def call
-    puts "Today's Broadway Shows"
     list_shows
+    menu
+    goodbye
   end
 
   def list_shows
+    puts "Today's Broadway Shows"
     puts <<-DOC.gsub /^\s*/, ''
     1. The Boys in the Band - from $89 - available through 5/30
     2. Harry Potter and the Cursed Child - from $40 - available through lottery
@@ -19,4 +21,37 @@ class BroadwayShows::CLI
     DOC
   end
 
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the show you would like to learn more about or type list to see all of the shows on or offer type exit."
+      input = gets.strip.downcase
+      case input
+    when "1"
+      puts "More info on show 1..."
+    when "2"
+      puts "More info on show 2..."
+    when "3"
+      puts "More info on show 3..."
+    when "4"
+      puts "More info on show 4..."
+    when "5"
+      puts "More info on show 5..."
+    when "6"
+      puts "More info on show 6..."
+    when "7"
+      puts "More info on show 7..."
+    when "8"
+      puts "More info on show 8..."
+    when "9"
+      puts "More info on show 9..."
+    when "list"
+      list_shows
+    end
+  end
+end
+
+  def goodbye
+    puts "Check back in soon to see more deals on the best shows in town!"
+  end
 end
